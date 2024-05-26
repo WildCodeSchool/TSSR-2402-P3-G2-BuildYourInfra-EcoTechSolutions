c0d210967b969c80d90df603cd12a2d0194e152d
Ingalls666 <162147143+Ingalls666@users.noreply.github.com>
2024-05-26 18:00:59 +0200

    Update Script_serverCore.ps1

--- ressource/Scripting/Script_serverCore.ps1
+++ ressource/Scripting/Script_serverCore.ps1
@@ -2,30 +2,38 @@ $FilePath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definitio
 
 $ConfigFile = "$FilePath\Config_Server_Core.csv"
 
-# Import du fichier de config
-$Config = Import-Csv -Path $ConfigFile -Delimiter "," -Header "Name","IP","Gateway","Domain","User","Password"
+# Import du fichier de configuration
+$Config = Import-Csv -Path $ConfigFile -Delimiter "," -Header "Name","IP","Gateway","Dns","Domain","User","Password"
 
-# creation des variables avec les donnees importées
+$errors = @()
+
+foreach ($server in $Config) {
+      try {
+# Création des variables avec les données importées
 $ServCore = $Config.Name 
 $IpServe = $Config.IP 
-$gateway = $Config.Gateway
+$Gateway = $Config.Gateway
+$Dns = $Config.Dns
 $Domain = $Config.Domain
 $User = $Config.User
 $Password = $Config.Password
 
-# Config adresse IP
-$interfaceIndex = (Get-NetAdapter).ifIndex
+# Configuration adresse IP
+$InterfaceIndex = (Get-NetAdapter).ifIndex[0]
 New-NetIPAddress `
 -IPAddress $IpServe `
 -PrefixLength 16 `
--InterfaceIndex $interfaceIndex `
--DefaultGateway $gateway
+-InterfaceIndex $InterfaceIndex `
+-DefaultGateway $Gateway `
+
+# Configuration du DNS
+Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses $Dns
 
 # Installation de l'AD-DS
 Install-WindowsFeature -Name RSAT-AD-Tools -IncludeManagementTools -IncludeAllSubFeature
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
 
-# Ajout le serveur au domain
+# Ajout du serveur au domain
 $Pass= ConvertTo-SecureString $Password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PScredential ("$User@$Domain", $Pass) 
 Add-Computer -DomainName $Domain -credential $credential
@@ -35,3 +43,19 @@ Rename-Computer -NewName $ServCore -Force
 
 # Restart du serveur
 Restart-Computer
+
+} 
+catch {
+      $errors + = [PSCustomObject]@{
+            ServerName = $Server.Name
+            ErrorMessage = $_.Exception.Message
+         }
+      }
+}
+if ($error.Count -gt 0) {
+      Write-Host "Les erreurs produites pendant l'execution sont :"
+      $errors | Format-Table -AutoSize
+      }
+else { 
+      Write-Host "Le script s'est executé sans erreurs."
+      }
